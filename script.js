aebbf8838c4d60ef2ecd084c31fbcc126640fe88
Nedim Okic <nedim.okic@gmail.com>
2019-11-19 21:39:33 +0100

    Boolean logic

--- script.js
+++ script.js
@@ -168,3 +168,20 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log(' John\'s BMI is higher than Marks\'s. ');
 }
+
+/*************************
+*  Boolean logic
+*/
+
+var firstName = 'John';
+var age = 20;
+
+if (age < 13) {
+    console.log( firstName + ' is a boy. ');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager. ');
+} else if (age >= 20 && age <30) {
+    console.log( firstName + ' is a young man. ');
+} else {
+    console.log( firstName + ' is a men. ');
+}
