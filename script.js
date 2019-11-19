dd017a364ccd0b1860d84dae68583431ec9c9d42
Nedim Okic <nedim.okic@gmail.com>
2019-11-19 21:26:03 +0100

    Variables and data types

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('Hello World!!!');
+
+/*************************
+* Variables and data types
+*/
+var firstName = 'John';
+console.log(firstName);
+
+var lastName ='Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+//Variable naming rules
+/*
+var 3years = 3;
+var john/mark = 'John and Mark';
+var if = 23;
+var delete = 23;
+var function = 23;
+*/
