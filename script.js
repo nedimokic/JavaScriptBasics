e85cc083298240b0a38e36d34014eb7d91c0a6d6
Nedim Okic <nedim.okic@gmail.com>
2019-11-19 21:34:18 +0100

    Operator precedence

--- script.js
+++ script.js
@@ -84,3 +84,38 @@ console.log(typeof yearMark);
 console.log(typeof 'Markis older than John');
 var x;
 console.log(typeof x);
+
+/*************************
+* Operator precedence
+*/
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x=y=(3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x,y);
+
+// More operators
+x = x * 2;
+console.log(x);
+x += 10;
+console.log(x);
+x--;
+console.log(x);
+x++;
+console.log(x);
+x++
+console.log(x);
