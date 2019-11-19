2eae817ae435520430bb169f774c25006006e2cf
Nedim Okic <nedim.okic@gmail.com>
2019-11-19 21:42:00 +0100

    The Ternary Operator and Switch Statements

--- script.js
+++ script.js
@@ -185,3 +185,59 @@ if (age < 13) {
 } else {
     console.log( firstName + ' is a men. ');
 }
+
+/*************************
+*  The Ternary Operator and Switch Statements
+*/
+
+var firstName = 'John';
+var age = 16;
+
+age >= 18 ? console.log(firstName + ' drink beer. ')
+: console.log(firstName + ' drink juice. ');
+
+var drink = age>= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+
+var age=19
+if (age >= 18) {
+    var drink = 'beer'; 
+    console.log(firstName + ' drink beer. ');
+} else {
+    var drink = 'juice';
+    console.log(firstName + ' drink juice. ');
+}
+
+// Switch statement
+
+var job = 'driver';
+switch(job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + 'teaches kids how to code. ');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon. ');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites. ');
+        break;
+    default:
+        console.log(firstName + ' does something else. ');
+}
+
+var age = 17;
+switch(true) {
+    case age < 13:
+        console.log( firstName + ' is a boy. ');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager. ');
+        break;
+    case age >= 20 && age < 30:
+        console.log( firstName + ' is a young man. ');
+        break;
+    default:
+        console.log( firstName + ' is a men. ');
+}
