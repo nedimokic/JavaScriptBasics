73bcab5bb8870a5a39fec974b5ec5752c005e85f
Nedim Okic <nedim.okic@gmail.com>
2019-11-19 21:48:23 +0100

    Truthy and falsy values and equality operators

--- script.js
+++ script.js
@@ -241,3 +241,25 @@ switch(true) {
     default:
         console.log( firstName + ' is a men. ');
 }
+
+/*************************
+*  Truthy and falsy values and equality operators
+*/
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+height = 23;
+
+if (height || height === 0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable has NOT been defined');
+}
+
+// Equality opertaors
+if (height == '23') {
+    console.log('The == operator does type coercion!');
+}
