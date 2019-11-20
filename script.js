db4cd99b27ef0ee66f700db0e35bc3de19dbf791
Nedim Okic <nedim.okic@gmail.com>
2019-11-20 22:04:41 +0100

    Arrays

--- script.js
+++ script.js
@@ -347,3 +347,37 @@ var whatDoYouDo = function(job, firstName) {
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('driver', 'Mike'));
 console.log(whatDoYouDo('retired', 'Jane'));
+
+/*************************
+*  Arrays
+*/
+
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', 'false'];
+
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(23));
+
+var isDesigner = john.indexOf('designer') === -1 ?
+'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
