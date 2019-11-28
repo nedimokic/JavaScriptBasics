a18acf4588d8a9ae53c1531d00d5e01138973285
Nedim Okic <nedim.okic@gmail.com>
2019-11-28 21:29:16 +0100

    Loops and iteration

--- script.js
+++ script.js
@@ -492,4 +492,56 @@ if (john.calcBMI() > mark.calcBMI()) {
     console.log('they have the same BMI');
 }
 
+/*************************
+*  Loops and iteration
+*/
+
+// For loop
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+for ( var i = 1; i <= 20; i++ ) {
+    console.log(i);
+}
+
+for ( var i = 1; i <=20; i+=2 ) {
+    console.log(i);
+}  
+
+// i = 0, 0 < 10 true, log i to the console, i++
+// i = 1, 1 < 10 true, log i to the console, i++
+// ...
+// i = 9, 9 < 10 true, log i to the console, i++
+// i = 10, 10 < 10 FALSE, exiti the loop!
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
+
+// While loop
+var i = 0;
+while(i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+
+// Continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+//Looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
+}
+
 
