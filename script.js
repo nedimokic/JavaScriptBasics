d0ba50c1043ac57471ca5081150d37ba0c5ca198
Nedim Okic <nedim.okic@gmail.com>
2019-11-28 20:12:47 +0100

    Coding challenge

--- script.js
+++ script.js
@@ -460,4 +460,36 @@ john.calcAge();
 console.log(john);
 
 
+// Coding challenge
+
+var john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;   
+    }
+
+}
+
+var mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+
+if (john.calcBMI() > mark.calcBMI()) {
+    console.log(john.fullName + ' has a higher BMI of ' + john.bmi);
+} else if (mark.bmi > john.bmi) {
+    console.log(mark.fullName + 'has a higher of ' +mark.bmi);
+} else {
+    console.log('they have the same BMI');
+}
+
 
